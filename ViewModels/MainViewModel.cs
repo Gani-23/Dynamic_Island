acd330233313f99c29e2923bbbee586f2f5adef4
Gani <saiganesh7989@gmail.com>
2024-08-21 18:34:18 +0530

    main

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using ReactiveUI;
+using System;
+using System.Reactive.Linq;
+
+namespace DynamicIsland.ViewModels
+{
+    public class MainViewModel : ReactiveObject
+    {
+        private string _time;
+        private string _date;
+        private readonly IDisposable _timerSubscription;
+
+        public string Time
+        {
+            get => _time;
+            set => this.RaiseAndSetIfChanged(ref _time, value);
+        }
+
+        public string Date
+        {
+            get => _date;
+            set => this.RaiseAndSetIfChanged(ref _date, value);
+        }
+
+        public MainViewModel()
+        {
+            // Set up a timer to update the time and date every second
+            var timer = Observable.Interval(TimeSpan.FromSeconds(1))
+                .ObserveOn(RxApp.MainThreadScheduler);
+
+            _timerSubscription = timer.Subscribe(_ => UpdateTimeAndDate());
+            UpdateTimeAndDate(); // Initialize immediately
+        }
+
+        private void UpdateTimeAndDate()
+        {
+            var now = DateTime.Now;
+            Time = now.ToString("hh:mm:ss tt"); // 12-hour clock with AM/PM
+            Date = now.ToString("MMMM d, yyyy");
+        }
+
+        ~MainViewModel()
+        {
+            _timerSubscription.Dispose();
+        }
+    }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,36 @@
+using System;
+using Avalonia;
+using Avalonia.Controls;
+using DynamicIsland.ViewModels;
+
+namespace DynamicIsland.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel(); // Set the DataContext to the MainViewModel
+        }
+
+        protected override void OnOpened(EventArgs e)
+        {
+            base.OnOpened(e);
+
+            // Retrieve the primary screen's bounds
+            var screen = Screens.Primary;
+            var screenBounds = screen.Bounds;
+
+            // Retrieve the dimensions of the window
+            var windowWidth = this.Bounds.Width;
+            var windowHeight = this.Bounds.Height;
+
+            // Calculate the position for the top center
+            var x = (screenBounds.Width - windowWidth) / 2;
+            var y = 0; // Position the window at the top
+
+            // Set the window's position
+            this.Position = new PixelPoint((int)x, (int)y);
+        }
+    }
+}
